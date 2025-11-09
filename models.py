6c03e1f9c2f1d7b5e4256c923bb8fa0a3b7dfee0
Fabrizio Solf <fabriziossr@gmail.com>
2025-11-09 17:26:17 +0100

    cambios

--- models.py
+++ models.py
@@ -7,3 +7,32 @@ Your name is FastAPI bot and you are a helpful
 chatbot responsible for teaching FastAPI to your users.
 Always respond in markdown.
 """
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def load_text_model():
+    pipe = pipeline(
+        "text-generation",
+        model="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+        torch_dtype=torch.bfloat16,
+        device=device,
+    )
+    return pipe
+
+def generate_text(pipe: Pipeline, prompt: str, temperature: float = 0.7) -> str:
+    messages = [
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": prompt},
+    ]
+    prompt = pipe.tokenizer.apply_chat_template(
+        messages, tokenize=False, add_generation_prompt=True
+    )
+    predictions = pipe(
+        prompt,
+        temperature=temperature,
+        max_new_tokens=128,
+        do_sample=False,
+        top_k=10,
+        top_p=0.95,
+    )
+    output = predictions[0]["generated_text"].split("</s>\n<|assistant|>\n")[-1]
+    return output
